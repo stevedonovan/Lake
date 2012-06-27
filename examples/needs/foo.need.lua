89dc308a60de64c1a2a46961e487e9617da15e14
steve donovan <steve.j.donovan@gmail.com>
2012-06-27 15:18:26 +0200

    Does not combine files by default: use COMBINE. Recursive file copies build output directory structure

--- examples/needs/foo.need.lua
+++ examples/needs/foo.need.lua
@@ -1,6 +1,6 @@
 --- variables for package foo
-FOO = [[D:\dev\lua\Lake\scratch\needs\foo]]
+FOO = PWD..'foo'
 FOO_INCLUDE_DIR = FOO
 FOO_LIB_DIR = FOO
-FOO_LIBS = 'foo_static'
+FOO_LIBS = 'foo'
 ----
